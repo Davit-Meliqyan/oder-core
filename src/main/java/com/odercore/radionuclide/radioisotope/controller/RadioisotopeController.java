3b94e86444d6e1109a8874afe39568149de06fb9
Davit <davidmeliqyan6@gmail.com>
2025-08-05 12:23:49 +0400

    test commit

--- src/main/java/com/odercore/radionuclide/radioisotope/controller/RadioisotopeController.java
+++ src/main/java/com/odercore/radionuclide/radioisotope/controller/RadioisotopeController.java
@@ -2,13 +2,20 @@ package com.odercore.radionuclide.radioisotope.controller;
 
 import com.odercore.common.controller.AbstractCrudController;
 import com.odercore.common.service.AbstractCrudService;
+import com.odercore.equipment.cyclotron.exitport.dto.ExitPortDto;
 import com.odercore.radionuclide.radioisotope.dto.request.patch.RadioisotopePatchDto;
 import com.odercore.radionuclide.radioisotope.dto.request.upsert.RadioisotopeUpsertDto;
 import com.odercore.radionuclide.radioisotope.dto.response.RadioisotopeDto;
 import com.odercore.radionuclide.radioisotope.entity.Radioisotope;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/radioisotopes")
@@ -23,5 +30,11 @@ public class RadioisotopeController extends AbstractCrudController<
     protected RadioisotopeController(
             AbstractCrudService<Radioisotope, RadioisotopeDto, RadioisotopeUpsertDto, RadioisotopePatchDto> service) {
         super(service);
+    }
+
+    @GetMapping("/test/")
+    public ResponseEntity<String> getExitPortsByCyclotronId() {
+        return ResponseEntity.ok("test"
+        );
     }
 }
